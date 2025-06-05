13bc3e55d21b3c563ab475c39ba281c49b9b65c5
Alex Eremie <146241194+ereit673@users.noreply.github.com>
2025-06-05 03:12:02 +0200

    Add API folder and serializers for Board, Task, Comment

--- core/urls.py
+++ core/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('kanban_app.urls')),
+    path('api/', include('kanban_app.api.urls')),
 ]
---
+++ kanban_app/api/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from kanban_app.models import Board, Task, Comment
+
+
+class BoardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Board
+        fields = '__all__'
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
